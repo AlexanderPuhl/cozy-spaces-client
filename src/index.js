b5ec2605107d58a9fbaa8132980492c5fbbcba4b
Alexander Puhl <apuhlswimmer@hotmail.com>
2018-12-15 11:59:36 -0700

    formatted index

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(<Router>
   <Provider store={store}>
     <Refocus>
-    <App />
+      <App />
     </Refocus>
   </Provider>
-  </Router>, document.getElementById('root'));
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
